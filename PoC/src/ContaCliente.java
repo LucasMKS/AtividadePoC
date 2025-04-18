e0c9670e2f8be96de8c69c3fe0c865e8c118f5d9
wesley-cardoso1 <wesley.p.cardoso@outlook.com>
2025-04-18 17:33:18 +0000

    fix(ContaCliente): corrigir erro na classe ContaCliente

--- PoC/src/ContaCliente.java
+++ PoC/src/ContaCliente.java
@@ -1,18 +1,11 @@
 public class ContaCliente {
-    // Atributos
-    private Cliente cliente;
     private int id;
     private String nome;
     private String cpf;
     private double saldo;
     private String tipoConta;
 
-    // Construtores
-    public ContaCliente(Cliente cliente, double saldoInicial) {
-        this.cliente = cliente;
-        this.saldo = saldoInicial;
-    }
-
+    // Construtor com ID
     public ContaCliente(int id, String nome, String cpf, double saldo, String tipoConta) {
         this.id = id;
         this.nome = nome;
@@ -22,14 +15,6 @@ public class ContaCliente {
     }
 
     // Getters e Setters
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
     public int getId() {
         return id;
     }
@@ -68,26 +53,5 @@ public class ContaCliente {
 
     public void setTipoConta(String tipoConta) {
         this.tipoConta = tipoConta;
-    }
-
-    // Método para subtrair valor do saldo
-    public double subtrai(double valor) {
-        if (valor <= saldo) {
-            saldo -= valor;
-            System.out.println("Novo saldo: R$" + saldo);
-        } else {
-            System.out.println("Saldo insuficiente. Saída cancelada.");
-        }
-        return saldo;
-    }
-
-    // Exibir dados em formato de string
-    @Override
-    public String toString() {
-        if (cliente != null) {
-            return cliente.getNome() + " (" + cliente.getId() + ") saldo=R$" + String.format("%.2f", saldo);
-        } else {
-            return nome + " (" + id + ") saldo=R$" + String.format("%.2f", saldo);
-        }
     }
 }
